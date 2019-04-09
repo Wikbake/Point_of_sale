940ea63923289c6c46bfc8edc381b64d3ae9df71
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-09 19:48:10 +0200

    Test class

---
+++ src/PointOfSale/Seller_GUITest.java
@@ -0,0 +1,17 @@
+package PointOfSale;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Seller_GUITest {
+
+
+    @Test
+    public void testScan() throws IOException {
+
+
+    }
+}

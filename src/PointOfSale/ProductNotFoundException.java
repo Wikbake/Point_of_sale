df228e4f7ecb4f59affcb34f0ccb3b33950ecb59
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-10 17:40:22 +0200

    Fixed bug in documentation

--- src/PointOfSale/ProductNotFoundException.java
+++ src/PointOfSale/ProductNotFoundException.java
@@ -1,7 +1,7 @@
 package PointOfSale;
 
 /**
- * Special exception that informs that there is no article in database witch connects with entered id.
+ * Special exception that informs that there is no article in database which connects with entered id.
  */
 
 public class ProductNotFoundException extends RuntimeException{

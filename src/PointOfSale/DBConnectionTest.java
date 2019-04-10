9065b90f0fff1aa747296dcc58c9b7ae920f81d3
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-10 17:39:13 +0200

    Simple tests of DBConnection, yet still in progress

--- src/PointOfSale/DBConnectionTest.java
+++ src/PointOfSale/DBConnectionTest.java
@@ -9,15 +9,19 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.sql.Connection;
-//import java.sql.ResultSet;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+/**
+ * Simple tests of <code>DBConnection</code>.
+ */
 
 public class DBConnectionTest {
 
     @InjectMocks private DBConnection dbConnection;
     @Mock private Connection mockConnection;
     @Mock private Statement mockStatement;
+    @Mock private ResultSet mockResultSet;
 
     @Before
     public void setUp() {
@@ -27,10 +31,11 @@ public class DBConnectionTest {
     @Test
     public void testMockDBConnection() throws Exception {
         Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
-        Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(123);
+        Mockito.when(mockConnection.createStatement().executeQuery(Mockito.any())).thenReturn(mockResultSet);
+        Mockito.when(mockResultSet.getInt(Mockito.anyString())).thenReturn(123);
 
-        ResultSet rs = dbConnection.executeQuery("SELECT * FROM article");
-        int value = rs.getInt("id");
+        ResultSet rs = dbConnection.executeQuery("");
+        int value = rs.getInt("");
         System.out.println(value);
         Assert.assertEquals(value, 123);
         Mockito.verify(mockConnection.createStatement(), Mockito.times(1));

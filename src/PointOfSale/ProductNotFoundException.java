6cb6c10ef078762cae8b18424fcc68ad0170cd0c
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-10 17:35:03 +0200

    Added note

--- src/PointOfSale/ProductNotFoundException.java
+++ src/PointOfSale/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package PointOfSale;
 
+/**
+ * Special exception that informs that there is no article in database witch connects with entered id.
+ */
+
 public class ProductNotFoundException extends RuntimeException{
 
 }
